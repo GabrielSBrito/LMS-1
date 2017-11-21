5b5eedd4410a488599ea809641a89b71394e21c7
Yan Santos <yan.santosf@gmail.com>
2017-11-21 13:43:40 -0200

    Mysql

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_lms',
         'USER': 'azure',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '54224',
     }
