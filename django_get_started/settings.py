04b1e49096695f7307f86a087773735ddd6ab31c
Yan Santos <yan.santosf@gmail.com>
2017-11-21 12:52:57 -0200

    mysql configugrado

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -18,9 +18,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
-        'USER': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_lms',
+        'USER': 'azure',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
@@ -36,11 +36,11 @@ LOGIN_URL = '/login'
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
