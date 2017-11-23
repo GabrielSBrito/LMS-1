34270878fc35aa2fd113be8f5ac8e0726a828c80
Yan Santos <yan.santosf@gmail.com>
2017-11-23 13:51:54 -0200

    Main adicionado

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -6,12 +6,13 @@ from datetime import datetime
 from django.conf.urls import patterns, url, include
 from app.forms import BootstrapAuthenticationForm
 
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'app.views.home', name='home'),
+    url(r'^$', 'main.views.index', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- main/templates/admin/_siswc/wc_menu.php
+++
@@ -1,1 +0,0 @@
-<li class="dashboard_nav_menu_li <?= strstr($getViewInput, 'templates/') ? 'dashboard_nav_menu_active' : ''; ?>"><a class="icon-folder-open" title="Templates" href="dashboard.php?wc=templates/home">Templates</a></li>
